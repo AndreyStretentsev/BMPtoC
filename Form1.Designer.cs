6287596be5f1607a11aa607bf59ac219008155a5
AndreyStretentsev <AndreyStretentsev@github.com>
2023-08-18 13:41:17 +0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace BMPtoCcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.Convert = new System.Windows.Forms.Button();
+            this.BMPaddressTextBox = new System.Windows.Forms.TextBox();
+            this.OpenFont = new System.Windows.Forms.Button();
+            this.FontAddressTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FontSizeCombo = new System.Windows.Forms.ComboBox();
+            this.ConvertFontBtn = new System.Windows.Forms.Button();
+            this.OutFormatComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "BMPFile";
+            this.OpenFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OpenFile.Location = new System.Drawing.Point(13, 20);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(111, 26);
+            this.OpenFile.TabIndex = 0;
+            this.OpenFile.Text = "Open BMP";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // Convert
+            // 
+            this.Convert.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Convert.Location = new System.Drawing.Point(311, 52);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(148, 26);
+            this.Convert.TabIndex = 1;
+            this.Convert.Text = "Convert BMP";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // BMPaddressTextBox
+            // 
+            this.BMPaddressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BMPaddressTextBox.Location = new System.Drawing.Point(129, 22);
+            this.BMPaddressTextBox.Name = "BMPaddressTextBox";
+            this.BMPaddressTextBox.Size = new System.Drawing.Size(330, 24);
+            this.BMPaddressTextBox.TabIndex = 2;
+            this.BMPaddressTextBox.TextChanged += new System.EventHandler(this.addressTextBox_TextChanged);
+            // 
+            // OpenFont
+            // 
+            this.OpenFont.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OpenFont.Location = new System.Drawing.Point(13, 84);
+            this.OpenFont.Name = "OpenFont";
+            this.OpenFont.Size = new System.Drawing.Size(111, 26);
+            this.OpenFont.TabIndex = 3;
+            this.OpenFont.Text = "Open Font";
+            this.OpenFont.UseVisualStyleBackColor = true;
+            this.OpenFont.Click += new System.EventHandler(this.OpenFont_Click);
+            // 
+            // FontAddressTextBox
+            // 
+            this.FontAddressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FontAddressTextBox.Location = new System.Drawing.Point(130, 84);
+            this.FontAddressTextBox.Name = "FontAddressTextBox";
+            this.FontAddressTextBox.Size = new System.Drawing.Size(330, 24);
+            this.FontAddressTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(28, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Font size, px:";
+            // 
+            // FontSizeCombo
+            // 
+            this.FontSizeCombo.FormattingEnabled = true;
+            this.FontSizeCombo.Items.AddRange(new object[] {
+            "8",
+            "10",
+            "11",
+            "12",
+            "14",
+            "16",
+            "20",
+            "24",
+            "30",
+            "40"});
+            this.FontSizeCombo.Location = new System.Drawing.Point(129, 124);
+            this.FontSizeCombo.Name = "FontSizeCombo";
+            this.FontSizeCombo.Size = new System.Drawing.Size(65, 21);
+            this.FontSizeCombo.TabIndex = 6;
+            this.FontSizeCombo.SelectedIndexChanged += new System.EventHandler(this.FontSizeCombo_SelectedIndexChanged);
+            // 
+            // ConvertFontBtn
+            // 
+            this.ConvertFontBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ConvertFontBtn.Location = new System.Drawing.Point(311, 119);
+            this.ConvertFontBtn.Name = "ConvertFontBtn";
+            this.ConvertFontBtn.Size = new System.Drawing.Size(148, 26);
+            this.ConvertFontBtn.TabIndex = 7;
+            this.ConvertFontBtn.Text = "Convert Font";
+            this.ConvertFontBtn.UseVisualStyleBackColor = true;
+            this.ConvertFontBtn.Click += new System.EventHandler(this.ConvertFontBtn_Click);
+            // 
+            // OutFormatComboBox
+            // 
+            this.OutFormatComboBox.FormattingEnabled = true;
+            this.OutFormatComboBox.Location = new System.Drawing.Point(130, 52);
+            this.OutFormatComboBox.Name = "OutFormatComboBox";
+            this.OutFormatComboBox.Size = new System.Drawing.Size(121, 21);
+            this.OutFormatComboBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label2.Location = new System.Drawing.Point(21, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Output format:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 156);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OutFormatComboBox);
+            this.Controls.Add(this.ConvertFontBtn);
+            this.Controls.Add(this.FontSizeCombo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FontAddressTextBox);
+            this.Controls.Add(this.OpenFont);
+            this.Controls.Add(this.BMPaddressTextBox);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.OpenFile);
+            this.Name = "Form1";
+            this.Text = "Font and BMP converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.TextBox BMPaddressTextBox;
+        private System.Windows.Forms.Button OpenFont;
+        private System.Windows.Forms.TextBox FontAddressTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox FontSizeCombo;
+        private System.Windows.Forms.Button ConvertFontBtn;
+        private System.Windows.Forms.ComboBox OutFormatComboBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMPtoCcode
+{
+    public partial class Form1 : Form
+    {
+        string bmpFileName;
+
+        string fontFileName;
+        int fontSize;
+
+        string applicationPath;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            FontSizeCombo.SelectedIndex = 0;
+            OutFormatComboBox.Items.Add("4 bpp");
+            OutFormatComboBox.Items.Add("24 bpp");
+            OutFormatComboBox.SelectedIndex = 0;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            applicationPath = Application.StartupPath.ToString();
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            bmpFileName = OpenFileDialog.FileName;
+            BMPaddressTextBox.Text = bmpFileName;
+            OpenFile.BackColor = Color.Green;
+        }
+
+        private void Put24bpp(StreamWriter sw, Bitmap bmp)
+        {
+            for (int col = 0; col < bmp.Height; col++)
+            {
+                sw.Write("\t");
+                for (int row = 0; row < bmp.Width; row++)
+                {
+                    Color pixel = bmp.GetPixel(row, col);
+                    sw.Write("0x" + string.Format("{0:X2}", pixel.R) + ", ");
+                    sw.Write("0x" + string.Format("{0:X2}", pixel.G) + ", ");
+                    sw.Write("0x" + string.Format("{0:X2}", pixel.B) + ", ");
+                }
+                sw.WriteLine("");
+            }
+        }
+
+        private void Put4bpp(StreamWriter sw, Bitmap bmp)
+        {
+            int bmpWidth = bmp.Width / 2 + bmp.Width % 2;
+
+            for (int col = 0; col < bmp.Height; col++)
+            {
+                sw.Write("\t");
+                int pcnt = 0;
+                for (int row = 0; row < bmpWidth; row++)
+                {
+                    int upper = (bmp.GetPixel(pcnt++, col).ToArgb() << 4) & 0x0F0;
+                    int lower = pcnt < bmp.Width ? (bmp.GetPixel(pcnt++, col).ToArgb()) & 0x0F : 0;
+                    sw.Write("0x" + string.Format("{0:X2}", (byte)(upper | lower)) + ", ");
+                }
+                sw.WriteLine("");
+            }
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(bmpFileName);
+
+            int start = bmpFileName.LastIndexOf('\\') + 1;
+            int end = bmpFileName.LastIndexOf('.');
+            string path = bmpFileName.Substring(0, end);
+            string fileName = bmpFileName.Substring(start, end - start);
+
+            using (StreamWriter fstream = new StreamWriter(applicationPath + path + ".c"))
+            {
+                fstream.WriteLine("/*");
+                fstream.WriteLine(" * " + fileName + ".c");
+                fstream.WriteLine(" *");
+                fstream.WriteLine(" */");
+                fstream.WriteLine("");
+                fstream.WriteLine("#include <bitmaps.h>");
+                fstream.WriteLine("");
+                fstream.WriteLine("const uint8_t " + fileName + "[] = {");
+                fstream.WriteLine("");
+
+                switch (OutFormatComboBox.SelectedIndex)
+                {
+                    case 0:
+                        Put4bpp(fstream, bmp);
+                        break;
+                    case 1:
+                        Put24bpp(fstream, bmp);
+                        break;
+                    default:
+                        break;
+                }
+
+                fstream.WriteLine("");
+                fstream.WriteLine("};");
+                fstream.WriteLine("");
+                fstream.WriteLine("const bitmap_info_t " + fileName + "_info = {");
+                fstream.WriteLine("");
+                fstream.WriteLine("\t.width = " + bmp.Width + ",");
+                fstream.WriteLine("\t.height = " + bmp.Height + ",");
+                fstream.WriteLine("\t.bitmap = " + fileName);
+                fstream.WriteLine("");
+                fstream.WriteLine("};");
+                fstream.WriteLine("");
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void addressTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConvertFontBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FontSizeCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fontSize = int.Parse(FontSizeCombo.GetItemText(FontSizeCombo.SelectedItem));
+        }
+
+        private void OpenFont_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            fontFileName = OpenFileDialog.FileName;
+            FontAddressTextBox.Text = fontFileName;
+            OpenFont.BackColor = Color.Green;
+        }
+    }
+}
